96f2dd149143d84c7f6c2b6211bc3eb3bfc4f6c2
yayanyang <yayanyang@gmail.com>
2012-06-24 19:29:47 +0800

    add mq library
    Signed-off-by: yayanyang <yayanyang@gmail.com>

---
+++ mq/assemblyinfo.lua
@@ -0,0 +1,13 @@
+assembly = 
+   {
+   -- the project name
+   name = "lemon-mq";
+   -- the project guid
+   guid = "{ 0x612c9e2e, 0x2341, 0x4a71, { 0x8a, 0x11, 0x3c, 0x2e, 0x8e, 0x58, 0xc8, 0x52 } }";
+   -- the project errorcode define
+   errorcode=
+      {
+	  INVALID_URL ="";
+	  INVALID_URL_TYPE="";
+      };
+   }
