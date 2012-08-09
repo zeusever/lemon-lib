368169df58972cb6ea3d2252536d02d914777a13
yayanyang <yayanyang@gmail.com>
2012-08-09 17:06:27 +0800

    add trace library
    Signed-off-by: yayanyang <yayanyang@gmail.com>

---
+++ trace/abi.h
@@ -0,0 +1,11 @@
+/**
+* 
+* @file     abi
+* @brief    Copyright (C) 2012  yayanyang All Rights Reserved 
+* @author   yayanyang
+* @version  1.0.0.0  
+* @date     2012/08/09
+*/
+#ifndef LEMON_TRACE_ABI_H
+#define LEMON_TRACE_ABI_H
+#endif //LEMON_TRACE_ABI_H
